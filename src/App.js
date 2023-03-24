8a700ae2339b5b3d27d3e1a9e29b5f323f5ff126
Filip Kovac <filip.kvch@gmail.com>
2023-03-24 11:57:17 +0100

    Random 1 and 0 values, new game button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import Mine from './components/Mine'
 
 export default function App() {
+
+  const [mines, setMines] = React.useState(allNewMines())
+
+  function allNewMines() {
+    const newMine = [];
+    for (let i = 0; i < 25; i++) {
+      newMine.push(Math.floor(Math.random() * 2))
+    }
+    return newMine;
+  }
+
+  const mineElements = mines.map(mine => <Mine value={mine} />)
+
+  function newGame() {
+    setMines(allNewMines())
+  }
+
   return (
-    <div>
-      Test
-    </div>
+    <main>
+      <div className='mine-container'>
+        {mineElements}
+      </div>
+      <button className='new-game' onClick={newGame}>New Game</button>
+    </main>
   )
 }
---
+++ src/components/Mine.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Mine(props) {
+    return (
+        <div className="mine-face">
+            <h3>
+                {props.value}
+            </h3>
+        </div>
+    )
+}
