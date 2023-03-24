12fe2332cd0961140bf962c8ae45e23fae2ffc2a
Filip Kovac <filip.kvch@gmail.com>
2023-03-24 17:15:25 +0100

    finishing touches

--- src/App.js
+++ src/App.js
@@ -53,7 +53,10 @@ export default function App() {
   function holdDice(id) {
     !tenzies && setDice(prevDice => prevDice.map(die => {
       return die.id === id ?
-        { ...die, isHeld: !die.isHeld } :
+        {
+          ...die,
+          isHeld: !die.isHeld
+        } :
         die
     }))
   }
@@ -86,10 +89,10 @@ export default function App() {
 
   return (
     <main>
-      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='dice-container'>
+        {tenzies && <Confetti />}
         {diceElements}
       </div>
       <button className='roll-dice' onClick={rollDice}>{tenzies ? "New game" : "Roll"}</button>
